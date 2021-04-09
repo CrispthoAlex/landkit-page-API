83f9b5d3a35679e77d7bb7baf7fb80d534c8e987
CrispthoAlex <alex877@gmail.com>
2021-04-09 01:12:21 -0500

    [Update] Landkit's scripts and styles were updated
    * Search script was updated
    	* Search in innerText attribute was applied
    	* Placeholder dynamic was added
    * Load more script was updated to show:
    	* 1) six elements more or,
    	* 2) All elements
    * Cards styles were updated
    	* price styles was updated; background dynamic.
    	* max heigth of card was modified

--- scripts/script.js
+++ scripts/script.js
@@ -71,16 +71,32 @@ $(function () {
   ]
   let start = new Date(2001, 0, 1)
   let end = new Date()
+  // Random date
   let ranDate = randomDate(start, end);
   let formatDate = months[ranDate.getMonth()].slice(0, 3) + ' ' + ranDate.getDate()
   let fullDate = formatDate + ', ' + ranDate.getFullYear();
   
-  let arrayfavorite = [];
   // Random genere
-  let genere = Math.random(0, 1) === 0? 'men' : 'women'; 
+  let genere;
   // Random image
-  let imgCard = Math.floor(Math.random()*2) === 0? 'tech' : Math.floor(Math.random()*2) === 1?'arch' : 'nature';
-  let price = Math.floor(Math.random()*100)
+  let imgCard;
+  let price;
+
+  function randomElements() {
+    // Random date
+    ranDate = randomDate(start, end);
+    formatDate = months[ranDate.getMonth()].slice(0, 3) + ' ' + ranDate.getDate()
+    fullDate = formatDate + ', ' + ranDate.getFullYear();
+
+    // Random genere
+    genere = Math.random(0, 1) === 0? 'men' : 'women'; 
+    // Random image
+    imgCard = Math.floor(Math.random()*3) === 0? 'tech' : Math.floor(Math.random()*3) === 1?'arch' : Math.floor(Math.random()*3) === 2? 'nature' : 'people';
+    price = Math.floor(Math.random()*100);
+  }
+
+  let arrayfavorite = [];
+  
   
   $.ajax({
     url: globalURL + 'posts', // Load the blogs
@@ -90,12 +106,13 @@ $(function () {
     success: function (posts) {
 
       $.each(posts, function (idx, post) {
+        randomElements();
         let postUserId = post.userId;
         
         $('.preview-blogs').append(
             `<article class="preview-blogs__card" id="${post.id}" aria-label="Preview blog">
               <figure class="preview-blogs__card-img" title="Add to Favorites">
-                <p class="pricing">$${price}/MO</p>
+                <p class="pricing" style="background-color:${Number(price) < 40? '#ff3f3f': Number(price) > 85? 'gold' : 'lightseagreen'}">$${price}/MO</p>
                 <svg class="favorite js-favorite" width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16 6.2c0 .2-.1.4-.3.5l-3.4 3.6.8 5v.2c0 .3-.1.5-.4.5l-.4-.1L8 13.5l-4.3 2.4-.4.1c-.3 0-.4-.2-.4-.5v-.2l.8-5L.2 6.7 0 6.2c0-.3.3-.4.5-.5L5.4 5 7.5.4c.1-.2.3-.4.5-.4s.4.2.5.4L10.6 5l4.9.7c.2 0 .5.2.5.5z" fill="#B4C2D3"/></svg>
                 <img  src="${post.imageBg + imgCard}" alt="">
               </figure>
@@ -119,7 +136,7 @@ $(function () {
       $(".preview-blogs__card").slice(0, 6).show();
 
       /* Favorite blog */
-      let cardlist = document.querySelectorAll('.preview-blogs__card');
+      let cardlist = $('.preview-blogs__card');
       let jsfavlist = document.querySelectorAll('.js-favorite');
 
       let jsparent; // Save the clicked parent element
@@ -141,6 +158,21 @@ $(function () {
 
       });
 
+      // Test: new search
+      const testSearch = $('#search-input');
+      //let testarr = [];
+      testSearch.on('keyup', (str) => {
+        const searchStr = str.target.value.toLowerCase();
+        // Filter the searching
+        const filteredBlogs = posts.responseJSON.filter( (searchIn) => {
+          return searchIn.name.toLowerCase().includes(searchStr) ||
+            searchIn.title.toLowerCase().includes(searchStr) ||
+            searchIn.body.toLowerCase().includes(searchStr) ||
+            searchIn.content.toLowerCase().includes(searchStr) ;
+          });
+        //console.log(filteredBlogs);
+      });
+
       /* Load current blog */
       let infoBlog;
       $('.js-overview__subtitle').on('click', function () {
@@ -148,8 +180,7 @@ $(function () {
 
         // Get the value of ID attribute. In this case, it's a number
         idpostHTML = jsparent.getAttribute('id');
-        idRight = Number(idpostHTML) - 1 // Fixing id position
-        infoBlog = posts.responseJSON[idRight];
+        infoBlog = posts.responseJSON.find(post => post.id == idpostHTML); // Get post id
 
         // Display the blog
         readyBlog() ;
@@ -194,7 +225,7 @@ $(function () {
       })
     }
   });
-  let arrsearch = [];
+  
   /**
    * Change section:
    * Click for favorite section, another click return to articles section
@@ -202,33 +233,34 @@ $(function () {
   // Show only favorite section
   $('.js-navigate-list__item-favorite').on('click', function () {
     
-    $('.preview-blogs').css('display','none');
     $('.favorites-blogs').css('display','flex');
-    $('.main-content__load-more').css('display','none');
-    $('.current-blog').css('display','none');
     // Check favorite blogs
     if ( $('.favorites-blogs').children().length === 0 ) {
       $('.page404').css('display','flex');
     }
+    $('.principal-section').css('display','');
+    $('.preview-box').css('display','none');
+    $('.current-blog').css('display','none');
+  
   });
+  
   // Show only preview section
   $('.js-navigate-list__item-preview').on('click', function () {
-    $('.preview-blogs').css('display','flex');
+  
+    $('.preview-box').css('display','block');
+    $('.principal-section').css('display','');
     $('.favorites-blogs').css('display','none');
-    $('.main-content__load-more').css('display','flex');
-    $('.current-blog').css('display','none');
     $('.page404').css('display','none');
+    $('.current-blog').css('display','none');
+  
   });
 
   /**
    * readyBlog: function to show blog to read for the user
   */
    function readyBlog() {
-    $('.main-content__search').css('display','none');
-    $('.preview-blogs').css('display','none');
-    $('.favorites-blogs').css('display','none');
-    $('.main-content__load-more').css('display','none');
-    $('.current-blog').css('display','flex');
+    $('.principal-section').css('display','none');
+    $('.current-blog').css('display','');
   }
   
   
@@ -236,24 +268,40 @@ $(function () {
    * Searching content function
    * This function search for content on the blogs
   */
+  let arrsearch = [];
+  let inputSearch;
+  
   $('.js-main-content__search-go').on('click', function () {
-    
-    let input = document.getElementById('search-content').value;
-    input = input.toLowerCase();
-
-    arrsearch = document.getElementsByClassName('overview__paragraph');
-
-    let searchElparent; // save each element to display not
-    for (i = 0; i < arrsearch.length ; i++) {
+    $('.page404').css('display','none');
+    inputSearch = $('#search-input');
+    //console.log(inputSearch[0].value)
+    if (inputSearch[0].value) {
+      // Reset placeholder
+      $('#search-input')[0].placeholder='Search' ;
       
-      if (!arrsearch[i].innerHTML.toLowerCase().includes(input)) {
-        searchElparent = $( arrsearch[i] ).parents('.preview-blogs__card')[0];
-        $(searchElparent).css('display', 'none') ;
+      inputSearch = inputSearch[0].value.toLowerCase();
+
+      arrsearch = $('.preview-blogs__card').each( element => {return element}) ;
+
+      let endSearch = [];
+
+      // Loop to show the matched elements
+      for (i = 0; i < arrsearch.length ; i++) {
+        // In new search, display the hide element
+        if ( arrsearch[i].style.display == 'none') arrsearch[i].style.display='block' ;
+
+        if (!arrsearch[i].innerText.toLowerCase().includes(inputSearch)) {
+          $(arrsearch[i]).css('display', 'none') ;
+        } else { endSearch.push(arrsearch[i]); }
+
       }
-    }
-    if (arrsearch.length === 0) {
-      $('.page404').css('display','flex');
-    }
+      // No elements, display 404 page
+      $('.main-content__search-results').html(`${endSearch.length} results`);
+      if (endSearch.length == 0) {
+        $('#search-input')[0].placeholder='Try again' ;
+        $('.page404').css('display','flex');
+      }
+    } else { $('#search-input')[0].placeholder='Type something' ; }
   });
 
   /* Funtion to add the objects to the localstorage */
@@ -267,11 +315,27 @@ $(function () {
   };
 
   /* Load more event */
+  let hiddenCards;
+  // Load only 6 elements on click
+  $("body").on('click touchstart', '.text', function (e) {
+    hiddenCards = $(".preview-blogs__card:hidden");
 
-  $("body").on('click touchstart', '.js-main-content__load-more', function (e) {
-    
-    $(".preview-blogs__card:hidden").slice(0, 6).slideDown();
-    if ($(".preview-blogs__card:hidden").length == 0) {
+    hiddenCards.slice(0, 6).slideDown();
+    if (hiddenCards.length == 0) {
+      $(".main-content__load-more").css('visibility', 'hidden');
+    }
+    $('html,body').animate({
+      scrollTop: $(this).offset().top
+    }, 1000);
+    (e).preventDefault();
+  });
+  
+  // Load only full elements on click
+  $("body").on('click touchstart', '.arrow', function (e) {
+    hiddenCards = $(".preview-blogs__card:hidden");
+    console.log(hiddenCards.length);
+    hiddenCards.slice(0, hiddenCards.length).slideDown();
+    if (hiddenCards.length == 0) {
       $(".main-content__load-more").css('visibility', 'hidden');
     }
     $('html,body').animate({
